cfa7bfa04c8ac92ecdf3ece98bcccdae912f34ea
Findlay Noble <findlaynoble@Normans-MacBook-Air.local>
2021-03-28 08:13:32 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Hello World');''
+
+  let firstName ='Findlay' , lastName = 'Noble';
+  console.log(firstName, lastName)
+
+  let intrestrate = 1;
+  console.log(intrestrate)
+  
+  let age = 30;
+  let isApproved = false;
+  let selectColor = null;
+  
+  let person = {
+    firstname: 'Findaly'
+  };
+  console.log(person)
+
+  function clickedButton(){
+      console.log('button pressed');
+  }
+
